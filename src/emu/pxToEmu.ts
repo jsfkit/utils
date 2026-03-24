c8d17a4f08874ba964454c6330206f469cf04838
Matt Riggott <matt.riggott@gmail.com>
2026-03-24 16:09:07 +0000

    Fix JSDoc indentation

--- src/emu/pxToEmu.ts
+++ src/emu/pxToEmu.ts
@@ -1,11 +1,11 @@
 const EMU = 12700; // 914400 / 72;
 
 /**
-* Convert a number from pixels to EMU (English Metric Units), assuming the Excel default of 72 DPI.
-* Refer to the JSF spec for documentation on the EMU.
-*
-* @param px A pixel size that should be converted to EMU.
-* @returns The same size computed to EMU.
+ * Convert a number from pixels to EMU (English Metric Units), assuming the Excel default of 72 DPI.
+ * Refer to the JSF spec for documentation on the EMU.
+ *
+ * @param px A pixel size that should be converted to EMU.
+ * @returns The same size computed to EMU.
  */
 export function pxToEmu (px: number) {
   return Math.round(px * EMU);
